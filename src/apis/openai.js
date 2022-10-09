52aee730f47bd842352ad74e9151e5a6455e30aa
Josh Bickett <josh@bickett.net>
2022-10-09 08:41:17 -0700

    Updates to element content changing

--- src/apis/openai.js
+++ src/apis/openai.js
@@ -1,23 +1,16 @@
 const OpenAI = require('openai-api');
-const OPENAI_API_KEY = '';
 
 const openai = new OpenAI(OPENAI_API_KEY);
 
-export const getCompletion = async (url, title, text) => {
+export const getCompletion = async (title) => {
   console.log('title in getCompletion: ', title);
-  const prompt = `Below are arrays of length 4 with funny text. 
-
-   ###
-   ['Bananas', 'Fly to the moon', 'memes!!!', 'Weird stuff!']
-   ###
-   ['Bears', 'Beets', 'Battlestar Galactica', 'Bacon']
-   ###
-   ['To', 'be', 'or', 'not', 'to', 'be']`;
+  const prompt = `Below is an array of titles and funny lists of works about the titles.
+[{title: "The Office | The Show's US Website",  list: ["Bears", "Beets", "Battlestar Galactica", "Bacon"]},{title: "10 great works of literature | newliterature.com ",  list: ["To", "be", "or", "not", "to be"], {title: "javascript get environment", list:  ["get", "out", "of","my","environment"]}, {title: "${title}", list:`;
   console.log('prompt:', prompt);
   const gptResponse = await openai.complete({
     engine: 'davinci',
     prompt: prompt,
-    maxTokens: 100,
+    maxTokens: 150,
     temperature: 0.9,
     topP: 1,
     presencePenalty: 0,
@@ -25,8 +18,26 @@ export const getCompletion = async (url, title, text) => {
     bestOf: 1,
     n: 1,
     stream: false,
-    stop: ['###'],
+    stop: ['}'],
   });
 
-  return gptResponse.data;
+  const response = gptResponse.data;
+  console.log('response: ', response);
+  if (response?.choices?.length > 0) {
+    // funnyArray = results.choices[0].text;
+    console.log('returning: ', response.choices[0].text);
+    // remove [ and ] from the response
+    const cleanResponse = response?.choices[0].text;
+    var pattern = "'";
+    let re = new RegExp(pattern, 'g');
+    const arrayResults = cleanResponse
+      .replace('[', '')
+      .replace(']', '')
+      .replace(re, '');
+    console.log('array results', arrayResults);
+
+    return arrayResults.split(',');
+  } else {
+    return [];
+  }
 };
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,31 +1,49 @@
+import { getCompletion } from '../../apis/openai';
 console.log('page loaded');
 
-export const changeAnchor = (element) => {
-  console.log('element: ', element);
+const DEBUG = false;
+export const changeAnchor = (element, array) => {
+  if (DEBUG) console.log('element: ', element);
+  if (DEBUG) console.log('array: ', array);
   const children = element?.children;
 
-  console.log('___');
+  if (DEBUG) console.log('___');
   if (children.length > 0) {
     for (let i = 0; i < children.length; i++) {
-      changeAnchor(children[i]);
+      changeAnchor(children[i], array);
     }
   }
-  // disable the anchor tag
-  if (element.nodeName === 'A') {
-    console.log('disabling anchor tag', element);
 
-    element.addEventListener('click', (e) => {
-      e.preventDefault();
-      element.textContent = 'hello!!';
-    });
-  }
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    const randomIndex = Math.floor(Math.random() * array.length);
+    if (element.nodeName === 'A') {
+      element.textContent = array[randomIndex];
+    }
+  });
 };
 
 setTimeout(() => {
+  console.log('changing anchor tags');
+  makeWeird();
+}, 2);
+
+const makeWeird = async () => {
+  console.log('getting completion');
   const title = document.title;
   console.log('title: ', title);
-
-  console.log('changing anchor tags');
-
-  changeAnchor(document.body);
-}, 1000);
+  let funnyArray = ['apples', 'and', 'bananas'];
+  try {
+    let results = await getCompletion(title);
+    console.log('results: ', results);
+    if (results?.length) {
+      if (results[0]) {
+        funnyArray = results;
+      }
+    }
+    changeAnchor(document.body, funnyArray);
+  } catch (e) {
+    console.log('error: ', e);
+    changeAnchor(document.body, funnyArray);
+  }
+};
