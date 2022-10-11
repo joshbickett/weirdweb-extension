3a6461117f9e84ef21098eeceb1bad5c3a0d3b1e
Josh Bickett <josh@bickett.net>
2022-10-11 07:22:41 -0700

    Add color loading

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,8 @@
 import { getCompletion, getNewContent } from '../../apis/openai';
 console.log('page loaded');
 
+// create a list of sites it works on: reddit, twitter, washingtonpost, bloomberg, linkedin, google.com
+
 const DEBUG = false;
 export const changeAnchor = (element, array) => {
   if (DEBUG) console.log('element: ', element);
@@ -13,8 +15,6 @@ export const changeAnchor = (element, array) => {
       changeAnchor(children[i], array);
     }
   }
-
-  const thing = { apple: 'I am an apple', banana: 'I am a banana' };
 
   element.addEventListener('click', (e) => {
     e.preventDefault();
@@ -42,12 +42,44 @@ export const changeContent = (element) => {
   element.addEventListener('click', async (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const newContent = await getNewContent(element.textContent);
-    console.log('new content: ', newContent);
-    element.textContent = newContent;
+    // const newContent = await getNewContent(element.textContent);
+    // console.log('new content: ', newContent);
+    // element.textContent = newContent;
+    // do a while loop that changes the color of the text for 5 seconds
+    colorLoad(element, 0);
 
     // element.textContent =
   });
+};
+
+const colorLoad = (element, index) => {
+  const colorArray = [
+    '#000000',
+    '#000b0f',
+    '#00171f',
+    '#00232e',
+    '#002f3e',
+    '#003b4e',
+    '#00475d',
+    '#00536d',
+    '#005f7c',
+    '#006b8c',
+    '#00779c',
+    '#1a84a5',
+    '#338fad',
+    '#4d9cb6',
+    '#66a9bf',
+    '#80b6c8',
+    '#99c3d1',
+    '#b3d0da',
+  ];
+  element.style.color = colorArray[index];
+
+  if (index < colorArray.length - 1) {
+    setTimeout(() => {
+      colorLoad(element, index + 1);
+    }, 50);
+  }
 };
 
 const makeWeird = async () => {
