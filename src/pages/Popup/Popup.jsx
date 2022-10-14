25942686deb519f58259cf0552e83d9cc3c18a4f
Josh Bickett <josh@bickett.net>
2022-10-13 21:27:41 -0700

    Add the robot for UI

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import logo from '../../assets/img/robot.png';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
@@ -7,18 +7,12 @@ const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ borderRadius: '25px' }}
+        />
       </header>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -145,21 +145,13 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/img/robot.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
       ],
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets/img/icon-34.png',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
+
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
       filename: 'newtab.html',
