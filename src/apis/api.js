89213c3d2477c4dc25310252551d6e3c5bb1d8d9
Josh Bickett <josh@bickett.net>
2022-10-23 12:52:34 -0700

    Add turtle mode

--- src/apis/api.js
+++ src/apis/api.js
@@ -93,6 +93,53 @@ export const getNewContent = async (text) => {
   }
 };
 
+export const getTurtleContent = async (text) => {
+  const cleanText = text.replace(/[^a-zA-Z ]/g, '');
+
+  const letterChangePrompt = `The following changes the original text to funny text about turtles: 
+[
+  {original: "Jimmy John", funnyEdited: "Jimmy Turtle"}, 
+  {original: "A database without dynamic memory allocation ", funnyEdited: "A database without dynamic TURTLES"}"} 
+  {original: "The best way to buy the products you love.", funnyEdited: "Products are just Turtles"} 
+  {original: "Billionaire Bankman-Fried Tries to Fix Crypto’s Hacking Problem", funnyEdited: "Billionaire Bankman-Fried buys 10,000 turtles"}
+  {original: "Markets Calling: Forget It, Tories, You Can Go It Alone", funnyEdited: "Markets calling: We need more turtles"}
+  {original: "${cleanText}", funnyEdited:`;
+  try {
+    const gptResponse = await openai.complete({
+      engine: 'davinci',
+      prompt: letterChangePrompt,
+      maxTokens: 150,
+      temperature: 0.9,
+      topP: 1,
+      presencePenalty: 0,
+      frequencyPenalty: 0,
+      bestOf: 1,
+      n: 1,
+      stream: false,
+      stop: ['}'],
+    });
+    const response = gptResponse.data;
+    if (DEBUG) console.log('response: ', response);
+    if (response?.choices?.length > 0) {
+      // funnyArray = results.choices[0].text;
+      if (DEBUG) console.log('returning: ', response.choices[0].text);
+      // remove [ and ] from the response
+      const cleanResponse = response?.choices[0].text;
+      var pattern = '"';
+      let re = new RegExp(pattern, 'g');
+      const arrayResults = cleanResponse
+        .replace('[', '')
+        .replace(']', '')
+        .replace(re, '');
+      if (DEBUG) console.log('array results', arrayResults);
+      return arrayResults.split(',');
+    }
+  } catch (e) {
+    console.log('e', e);
+    return [];
+  }
+};
+
 export const getBackground = async () => {
   const response = await fetch(
     `https://lexica.art/api/v1/search?q=simple color`
@@ -103,9 +150,31 @@ export const getBackground = async () => {
   return selectAtRandom(images, 0);
 };
 
+export const getTurtleBackground = async () => {
+  console.log('get turtle background');
+  const response = await fetch(
+    `https://lexica.art/api/v1/search?q=simple turtle image`
+  );
+  const data = await response.json();
+  if (DEBUG) console.log('data', data);
+  const images = data.images;
+  return selectAtRandom(images, 0);
+};
+
 export const getImage = async (search) => {
   // make a GET request to the following endpoint => https://lexica.art/api/v1/search?q=apples
   const url = `https://lexica.art/api/v1/search?q=${search}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log('data', data);
+  const images = data.images;
+  return selectAtRandom(images, 0);
+};
+
+export const getTurtleImage = async (search) => {
+  // make a GET request to the following endpoint => https://lexica.art/api/v1/search?q=apples
+  const url = `https://lexica.art/api/v1/search?q=${search} and include turtles`;
 
   const response = await fetch(url);
   const data = await response.json();
